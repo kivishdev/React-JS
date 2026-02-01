789b5e439cf5b5bd274bf8888dcf2919e39f7fa9
Kirti Yadav <kirtiy380@gmail.com>
2026-02-01 15:17:54 +0530

    React JS Learning

---
+++ Import and Export/app.js
@@ -0,0 +1,13 @@
+var a = 60
+// Named Export:
+export var b = 75 
+
+var c = 65
+export default a;
+// Ab hum ise dusre files me access kr paaye isliye krengee export.
+
+/* Export do type k hote Haii.
+ Named: Ek hi cheez export krnaa 
+ 
+ Default: Ek saath bohot cheeze export krnaa.
+  */
---
+++ Import and Export/script.js
@@ -0,0 +1,7 @@
+import a from './app.js';
+// Named vaala export use kiya tha isme.
+import {b} from './app.js';
+
+console.log(a);
+console.log(b);
+
