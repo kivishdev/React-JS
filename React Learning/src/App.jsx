f10ff252447d0ba4a1ae0f572e1adc20944a340b
Kirti Yadav <kirtiy380@gmail.com>
2026-02-03 16:17:26 +0530

    Learning components.

--- React Learning/src/App.jsx
+++ React Learning/src/App.jsx
@@ -1,20 +1,14 @@
+import React from "react";
+import CardPracice  from "./components/01_JSX/card";
+
 function ReactApp() {
   return (
+    // Isko bolte hai fragment.
     <>
       <div id="dad">Hello 1</div>
       <div>Hello 2</div>
       <div id="chacha">Hello Chacha</div>
-
-      <div className="card">
-        <h1>Hello Kirti Yadav</h1>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam,
-          quam temporibus debitis cum amet reiciendis odit qui aperiam at neque
-          voluptas eveniet sed, doloribus ipsum excepturi exercitationem
-          similique natus non aliquam animi iste ducimus. Facilis, illum
-          eveniet.
-        </p>
-      </div>
+      <CardPracice />
     </>
   );
 }
---
+++ React Learning/src/components/01_JSX/card.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    <div>
+        <div className="card">
+        <h1>Hello Kirti Yadav</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam,
+          quam temporibus debitis cum amet reiciendis odit qui aperiam at neque
+          voluptas eveniet sed, doloribus ipsum excepturi exercitationem
+          similique natus non aliquam animi iste ducimus. Facilis, illum
+          eveniet.
+        </p>
+        </div>
+    </div>
+  )
+}
+
+export default card
