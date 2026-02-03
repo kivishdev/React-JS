0fc9dea38fe6bc146136650d150b4bec1eaf8d49
Kirti Yadav <kirtiy380@gmail.com>
2026-02-03 13:48:38 +0530

    Exploring React adding normal html and css.

--- React Learning/src/App.jsx
+++ React Learning/src/App.jsx
@@ -1,13 +1,22 @@
-// App.jsx k papa main.jsx hai.
+function ReactApp() {
+  return (
+    <>
+      <div id="dad">Hello 1</div>
+      <div>Hello 2</div>
+      <div id="chacha">Hello Chacha</div>
 
+      <div className="card">
+        <h1>Hello Kirti Yadav</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam,
+          quam temporibus debitis cum amet reiciendis odit qui aperiam at neque
+          voluptas eveniet sed, doloribus ipsum excepturi exercitationem
+          similique natus non aliquam animi iste ducimus. Facilis, illum
+          eveniet.
+        </p>
+      </div>
+    </>
+  );
+}
 
-function react() {
-  // return <h1>Hello React</h1> Normal return kiyaa.
-  // Fragments:  Ek emply tag jiske andar hum multiple things return kr skte hai.
-   return <>    
-  <div id="dad">Hello 1</div>
-  <div>Hello 2</div>
-  <div id="chacha"> Hello Chacha</div>
-  </>
-} 
-export default react
+export default ReactApp;
