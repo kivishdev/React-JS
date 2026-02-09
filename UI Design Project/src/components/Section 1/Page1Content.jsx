54a8ecc82c3152444ad6c4051b44757358564c9a
Kirti Yadav <kirtiy380@gmail.com>
2026-02-10 01:27:27 +0530

    Removing Scrollbar using CSS with webkit.

--- UI Design Project/src/components/Section 1/Page1Content.jsx
+++ UI Design Project/src/components/Section 1/Page1Content.jsx
@@ -3,10 +3,9 @@ import LeftContent from './LeftContent.jsx'
 import RightContent from './RightContent.jsx'
 
 const Page1Content = (props) => {
-  console.log('Ye rhaa Page1 Contents ka',props.users);
   
   return (
-    <div className='px-10 py-10 flex gap-10 items-center justify-between h-[80vh] bg-white'>
+    <div className=' flex gap-10 items-center justify-between h-[80vh] bg-white'>
       <LeftContent />
       <RightContent users = {props.users}/>
     </div>
--- UI Design Project/src/components/Section 1/RightContent.jsx
+++ UI Design Project/src/components/Section 1/RightContent.jsx
@@ -4,7 +4,7 @@ import RightCard from './RightCard.jsx'
 const RightContent = (props) => {
   
   return (
-    <div className='h-full w-2/3 p-4 flex flex-nowrap gap-5 overflow-scroll bg-white'>
+    <div id='right' className='h-full w-2/3 p-4 flex flex-nowrap gap-5 overflow-scroll bg-white'>
       {props.users.map(function (data, idx) {
         return <RightCard key = {idx} CardNum = {data.num} CardImg = {data.img} CardTag = {data.tag} CardCss = {data.css}/>
       })}
