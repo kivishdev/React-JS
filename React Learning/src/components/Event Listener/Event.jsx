3e459c3381e2c8a22deba3eb696a2e8f71d2950d
Kirti Yadav <kirtiy380@gmail.com>
2026-02-10 22:53:05 +0530

    Understanding Hooks.

--- React Learning/src/components/Event Listener/Event.jsx
+++ React Learning/src/components/Event Listener/Event.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
 
 const Event = () => {
-
     function Btnclick() {     // Event trigger pe ye function chalega
-        console.log("Hello Duniya");
+        console.log("Hello");
     }
   return (
+    <>
     <div>
         <button onClick={Btnclick}>Click Here</button>        {/* Event Listener Lgaana */}
+
+        {/* 2nd Tarika: Function bnaake likhna. */}
+        <button className='bg-black' onClick={function() {
+          console.log("Dab GYaa Button");
+        }}>Button Dabaao Yahaa...</button>
+
+        {/* Input Event: */}
+        <input onChange={function (val) {
+          console.log(val.target.value);
+        }} type="text" placeholder='Enter Name'/>
     </div>
+    </>
   )
 }
 
